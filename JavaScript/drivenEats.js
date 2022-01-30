b6301114d0d03749516cfde1782ccdc3b02a49d3
Teones <teones.alex.talff99@gmail.com>
2022-01-30 18:35:57 -0300

    falta apenas calcular o valor final

--- JavaScript/drivenEats.js
+++ JavaScript/drivenEats.js
@@ -1,6 +1,9 @@
 let escolheuPrato
+let valorPrato
 let escolheuBebida
+let valorBebida
 let escolheuSobremesa
+let valorSobremesa
 
 function escolherPrato(prato) {
     let selecionado = document.querySelector(".pratos .selecionado")
@@ -8,6 +11,9 @@ function escolherPrato(prato) {
         selecionado.classList.toggle("selecionado")
     }
     prato.classList.add("selecionado")
+
+    let valor = document.querySelector(".pratos .selecionado .preco .valor")
+    valorPrato = parseFloat(valor.innerHTML).toFixed(2)
     
     escolheuPrato = "sim"
     ativarBotao()
@@ -19,6 +25,9 @@ function escolherBebida(bebida) {
     }
     bebida.classList.add("selecionado")
 
+    let valor = document.querySelector(".bebidas .selecionado .preco .valor")
+    valorBebida = parseFloat(valor.innerHTML).toFixed(2)
+
     escolheuBebida = "sim"
     ativarBotao()
 }
@@ -28,6 +37,9 @@ function escolherSobremesa(sobremesa) {
         selecionado.classList.toggle("selecionado")
     }
     sobremesa.classList.add("selecionado")
+
+    let valor = document.querySelector(".sobremesas .selecionado .preco .valor")
+    valorSobremesa = parseFloat(valor.innerHTML).toFixed(2)
 
     escolheuSobremesa = "sim"
     ativarBotao()
@@ -42,5 +54,18 @@ function ativarBotao() {
 }
 
 function finalizarPedido() {
+    let pratoEscolhido = document.querySelector(".pratos .selecionado h1")
+    let bebidaEscolhida = document.querySelector(".bebidas .selecionado h1")
+    let sobremesaEscolhida = document.querySelector(".sobremesas .selecionado h1")
     
+    let nome
+    let endereco
+    if (escolheuPrato == "sim" && escolheuBebida == "sim" && escolheuSobremesa == "sim") {
+        nome = prompt("Qual o seu nome?")
+        endereco = prompt("Qual o seu endereço?")
+    }
+
+    let mensagem =`Olá, gostaria de fazer o pedido:\n   - Prato: ${pratoEscolhido.innerHTML}\n   - Bebida: ${bebidaEscolhida.innerHTML}\n   - Sobremesa: ${sobremesaEscolhida.innerHTML}\n
+    Nome: ${nome}\n    Endereço: ${endereco}`
+    window.open("https://wa.me/+558399422831?text=" + encodeURIComponent(mensagem))
 }
